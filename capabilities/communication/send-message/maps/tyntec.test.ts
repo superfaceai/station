d49ff4735e8ffb126e2665508d00b5031a79ce90
Z <zdne@users.noreply.github.com>
2021-04-22 10:13:29 +0200

    chore: tyntec tests and fixes

---
+++ capabilities/communication/send-message/maps/tyntec.test.ts
@@ -0,0 +1,36 @@
+import { SuperfaceClient } from '@superfaceai/sdk';
+
+const recipient = process.env.COMMUNICATION_SENDMESSAGE_TYNTEC_TO
+let profile: any;
+let provider: any;
+
+describe('communication/send-message/tyntec', () => {
+  beforeAll(async () => {
+    const client = new SuperfaceClient;
+    profile = await client.getProfile('communication/send-message');
+    provider = await client.getProvider('tyntec');
+  })
+
+  it('sends a message', async () => {
+    const useCase = profile.getUseCase('SendMessage');
+    const result = await useCase.perform({ to: recipient, from: 'tyntec', text: 'Hello World!', channel: 'sms' }, { provider });
+    expect(result.isOk()).toBeTruthy();
+    expect(typeof(result.unwrap() as any).messageId).toBe('string');
+  });
+  
+  it('retrieves message status', async () => {
+    const sendMessageUseCase = profile.getUseCase('SendMessage');
+    const sendMessageResult = await sendMessageUseCase.perform({ to: recipient, from: 'tyntec', text: 'Hello World!', channel: 'sms' }, { provider });
+    const messageId = sendMessageResult.unwrap().messageId;
+
+    const useCase = profile.getUseCase('RetrieveMessageStatus');
+    const result = await useCase.perform({ messageId: messageId }, { provider });
+
+    // if (result.isErr()) {
+    //   console.log('error', result.error)
+    // }
+    
+    expect(result.isOk()).toBeTruthy();
+    expect(typeof(result.unwrap() as any).deliveryStatus).toBe('string');
+  });
+})
