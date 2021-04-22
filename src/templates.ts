1e1a68d6f53455fe8b37a60e8ef59265bb6796ce
Z <zdne@users.noreply.github.com>
2021-04-22 14:56:48 +0200

    Fix indentation

--- src/templates.ts
+++ src/templates.ts
@@ -50,17 +50,17 @@ export function mapTestTemplate(
   return `import { SuperfaceClient } from '@superfaceai/sdk';
 
 describe('${scope}/${usecase}/${provider}', () => {
-    it('performs correctly', async () => {
-        const client = new SuperfaceClient;
-        const profile = await client.getProfile('${scope}/${usecase}');
-        const useCase = profile.getUseCase('${kebabToCamelCase(usecase)}');
-        const provider = await client.getProvider('${provider}');
+  it('performs correctly', async () => {
+    const client = new SuperfaceClient;
+    const profile = await client.getProfile('${scope}/${usecase}');
+    const useCase = profile.getUseCase('${kebabToCamelCase(usecase)}');
+    const provider = await client.getProvider('${provider}');
 
-        expect(useCase).not.toBeUndefined()
-        expect(provider).not.toBeUndefined()
-        //Edit expected value
-        //await expect(useCase.perform({}, { provider })).resolves.toEqual()
-    })
+    expect(useCase).not.toBeUndefined()
+    expect(provider).not.toBeUndefined()
+    //Edit expected value
+    //await expect(useCase.perform({}, { provider })).resolves.toEqual()
+  })
 })
 `;
 }
