7431576d0339e8fe80aae31a65b938b9ddf08730
Z <zdne@users.noreply.github.com>
2021-04-22 13:37:17 +0200

    Twilio tests & origin

--- capabilities/communication/send-message/maps/twilio.test.ts
+++ capabilities/communication/send-message/maps/twilio.test.ts
@@ -4,33 +4,33 @@ const recipient = process.env.COMMUNICATION_SENDMESSAGE_TO
 let profile: any;
 let provider: any;
 
-describe('communication/send-message/tyntec', () => {
+describe('communication/send-message/twilio', () => {
   beforeAll(async () => {
     const client = new SuperfaceClient;
     profile = await client.getProfile('communication/send-message');
-    provider = await client.getProvider('tyntec');
+    provider = await client.getProvider('twilio');
   })
 
   it('sends a message', async () => {
     const useCase = profile.getUseCase('SendMessage');
-    const result = await useCase.perform({ to: recipient, from: 'tyntec', text: 'Hello World!', channel: 'sms' }, { provider });
+    // note: `from` input is configured in super.json
+    const result = await useCase.perform({ to: recipient, text: 'Hello World!', channel: 'sms' }, { provider });
     expect(result.isOk()).toBeTruthy();
     expect(typeof(result.unwrap() as any).messageId).toBe('string');
   });
-  
+
   it('retrieves message status', async () => {
     const sendMessageUseCase = profile.getUseCase('SendMessage');
-    const sendMessageResult = await sendMessageUseCase.perform({ to: recipient, from: 'tyntec', text: 'Hello World!', channel: 'sms' }, { provider });
+    const sendMessageResult = await sendMessageUseCase.perform({ to: recipient, text: 'Hello World!', channel: 'sms' }, { provider });
     const messageId = sendMessageResult.unwrap().messageId;
 
     const useCase = profile.getUseCase('RetrieveMessageStatus');
     const result = await useCase.perform({ messageId: messageId }, { provider });
 
-    // if (result.isErr()) {
-    //   console.log('error', result.error)
-    // }
-    
+    if (result.isErr()) {
+      console.log('error', result.error)
+    }    
     expect(result.isOk()).toBeTruthy();
     expect(typeof(result.unwrap() as any).deliveryStatus).toBe('string');
-  });
+  });  
 })
