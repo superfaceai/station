28d39b3243d369092fa13a6f8285a47b3a0523fa
Z <zdne@users.noreply.github.com>
2021-04-23 11:23:38 +0200

    PLIVO WIP
    Note there is a bug that prevents us to use poorly designed Plivo's API: The trailing slash in URL is stripped and as such the call to send message results in 404.

--- capabilities/communication/send-message/maps/plivo.test.ts
+++ capabilities/communication/send-message/maps/plivo.test.ts
@@ -1,15 +1,25 @@
 import { SuperfaceClient } from '@superfaceai/sdk';
 
-describe('communication/send-message/plivo', () => {
-    it('performs correctly', async () => {
-        const client = new SuperfaceClient;
-        const profile = await client.getProfile('communication/send-message');
-        const useCase = profile.getUseCase('sendMessage');
-        const provider = await client.getProvider('plivo');
+const recipient = process.env.COMMUNICATION_SENDMESSAGE_TO
+let profile: any;
+let provider: any;
 
-        expect(useCase).not.toBeUndefined()
-        expect(provider).not.toBeUndefined()
-        //Edit expected value
-        //await expect(useCase.perform({}, { provider })).resolves.toEqual()
-    })
+describe('communication/send-message/plivo', () => {
+  beforeAll(async () => {
+    const client = new SuperfaceClient;
+    profile = await client.getProfile('communication/send-message');
+    provider = await client.getProvider('plivo');
+  })
+
+  it('sends a message', async () => {
+    const useCase = profile.getUseCase('SendMessage');
+    const result = await useCase.perform({ to: recipient, from: 'plivotest', text: 'Hello World!' }, { provider });
+
+    if (result.isErr()) {
+      console.log('Error >', result.error);
+    }
+    expect(result.isOk()).toBeTruthy();
+    expect(typeof(result.unwrap() as any).messageId).toBe('string');
+    console.log('Result >', result.value)
+  });
 })
