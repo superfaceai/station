8463065183280c1c5c1204c5509549110bcf2464
Ondřej Musil <o@superface.ai>
2021-04-26 11:03:36 +0200

    feat: generate types with CLI 0.0.9

--- superface/sdk.ts
+++ superface/sdk.ts
@@ -1,6 +1,7 @@
 import { createTypedClient } from "@superfaceai/one-sdk";
 import { communicationSendEmail } from "./types/communication/send-email";
 import { communicationSendMessage } from './types/communication/send-message';
+export { CommunicationSendEmailProfile } from "./types/communication/send-email";
 export const typeDefinitions = {
     ...communicationSendEmail,
     ...communicationSendMessage
