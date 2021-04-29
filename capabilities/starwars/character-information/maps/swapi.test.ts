b48b00a6a5f699babe83d8305427900819059182
Stephen Mizell <smizell@gmail.com>
2021-04-29 14:37:19 -0500

    Add Star Wars capability

---
+++ capabilities/starwars/character-information/maps/swapi.test.ts
@@ -0,0 +1,23 @@
+
+import { SuperfaceClient } from '@superfaceai/one-sdk';
+
+describe('starwars/character-information/swapi', () => {
+  it('performs correctly', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('starwars/character-information');
+    const useCase = profile.getUseCase('RetrieveCharacterInformation');
+    const provider = await client.getProvider('swapi');
+
+    expect(useCase).not.toBeUndefined();
+    expect(provider).not.toBeUndefined();
+    const result = await useCase.perform({
+      characterName: "Luke Skywalker"
+    });
+    expect(result.unwrap()).toEqual({
+      height: "172",
+      name: "Luke Skywalker",
+      weight: "77",
+      yearOfBirth: "19BBY",
+    });
+  })
+})
