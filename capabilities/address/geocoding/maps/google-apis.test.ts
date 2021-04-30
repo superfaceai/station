d0bcd314629830d31461d7dfb64fc29f1939a2e9
Z <zdne@users.noreply.github.com>
2021-04-30 14:56:55 +0200

    Broken generated interface ?

--- capabilities/address/geocoding/maps/google-apis.test.ts
+++ capabilities/address/geocoding/maps/google-apis.test.ts
@@ -17,4 +17,22 @@ describe('address/geocoding/google-apis', () => {
     expect(result.unwrap().latitude).toBe(37.4305);
     expect(result.unwrap().longitude).toBe(-122.0769661);
   })
+
+  it('Reverse geocode geographical coordingates', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('address/geocoding');
+    const provider = await client.getProvider('google-apis');
+    const result = await profile.useCases.ReverseGeocode.perform({
+      latitude: 40.714224,
+      longitude: -73.961452
+    }, { provider })
+
+    const value = result.unwrap()
+    expect(result.isOk()).toBeTruthy();
+    expect(value.length()).toBeGreaterThan(0);
+    // expect(value.streetAddress).toBe("");
+    // expect(value.addressLocality).toBe("");
+    // expect(value.addressRegion).toBe("");
+    // expect(value.addressCountry).toBe("");
+  })  
 })
--- superface/types/address/geocoding.ts
+++ superface/types/address/geocoding.ts
@@ -62,37 +62,6 @@ export interface ReverseGeocodeInput {
      **/
     longitude: unknown;
 }
-/**
- * Reverse geocode
- * Decodes geographical coordinates (latitude and longitude) into an address
- **/
-export interface ReverseGeocodeResult {
-    /**
-     * Country
-     * The country. For example, USA. You can also provide the two-letter ISO 3166-1 alpha-2 country code.
-     **/
-    addressCountry?: unknown;
-    /**
-     * Locality
-     * The locality in which the street address is, and which is in the region. For example, Mountain View.
-     **/
-    addressLocality?: unknown;
-    /**
-     * Region
-     * The region in which the locality is, and which is in the country. For example, California or another appropriate first-level Administrative division
-     **/
-    addressRegion?: unknown;
-    /**
-     * Postal code
-     * The postal code. For example, 94043.
-     **/
-    postalCode?: unknown;
-    /**
-     * Street address
-     * The street address. For example, 1600 Amphitheatre Pkwy.
-     **/
-    streetAddress?: unknown;
-}
 export const profile = {
     "Gecode": typeHelper<GecodeInput, GecodeResult>(),
     "ReverseGeocode": typeHelper<ReverseGeocodeInput, ReverseGeocodeResult>()
