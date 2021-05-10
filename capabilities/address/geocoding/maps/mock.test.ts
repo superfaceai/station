46c7479c69a32843afa7a83bd1abe47ff4abb424
Z <zdne@users.noreply.github.com>
2021-05-10 13:40:48 +0200

    Mock provider origin

---
+++ capabilities/address/geocoding/maps/mock.test.ts
@@ -0,0 +1,35 @@
+import { SuperfaceClient } from '../../../../superface/sdk';
+
+describe('address/geocoding/opencage-typed', () => {
+
+  it('Geocode address', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('address/geocoding');
+    const provider = await client.getProvider('mock');
+    const result = await profile.useCases.Gecode.perform({
+      streetAddress: "",
+      addressLocality: "",
+      addressRegion: "",
+      addressCountry: ""
+    }, { provider })
+
+    const value = result.unwrap()
+    expect(result.isOk()).toBeTruthy();
+    expect(value.latitude).toBeDefined();
+    expect(value.longitude).toBeDefined();
+  })
+
+  it('Reverse geocode geographical coordingates', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('address/geocoding');
+    const provider = await client.getProvider('mock');
+    const result = await profile.useCases.ReverseGeocode.perform({
+      latitude: 0,
+      longitude: 0
+    }, { provider })
+
+    const value = result.unwrap()
+    expect(result.isOk()).toBeTruthy();
+    expect(value[0]).toBeDefined();
+  })
+})
