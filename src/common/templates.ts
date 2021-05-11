75f4a7aaaa2e284f68d23462dd2a4cbdaa43814a
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-05-11 11:17:01 +0200

    chore: different test timeout setting

--- src/common/templates.ts
+++ src/common/templates.ts
@@ -53,6 +53,10 @@ export function mapTestTemplate(
   return `import { SuperfaceClient } from '../../../../superface/sdk';
 
 describe('${scope}/${usecase}/${provider}-typed', () => {
+  beforeAll(() => {
+    jest.setTimeout(10000)
+  })
+  
   it('performs correctly', async () => {
     const client = new SuperfaceClient();
     const profile = await client.getProfile('${scope}/${usecase}');
@@ -65,6 +69,6 @@ describe('${scope}/${usecase}/${provider}-typed', () => {
     //Edit input values and expected result
     //const result = await usecase.perform({}, { provider });
     //expect(result.unwrap()).toEqual();
-  }, 10000);
+  });
 });`;
 }
