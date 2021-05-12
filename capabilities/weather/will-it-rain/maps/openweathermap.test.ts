f376fe8d644d2e2c62b9dc2ff9e1b607b508673c
Jan Halama <jan.halama@superface.ai>
2021-05-12 14:52:48 +0200

    test(weather): add openweathermap provider test file

--- capabilities/weather/will-it-rain/maps/openweathermap.test.ts
+++ capabilities/weather/will-it-rain/maps/openweathermap.test.ts
@@ -1,7 +1,7 @@
 import { SuperfaceClient } from '../../../../superface/sdk';
 
 describe('weather/will-it-rain/openweathermap-typed', () => {
-  it('performs correctly', async () => {
+  it('should define provider and usecase', async () => {
     const client = new SuperfaceClient();
     const profile = await client.getProfile('weather/will-it-rain');
     const provider = await client.getProvider('openweathermap');
@@ -9,9 +9,26 @@ describe('weather/will-it-rain/openweathermap-typed', () => {
 
     expect(provider).not.toBeUndefined();
     expect(usecase).not.toBeUndefined();
+  });
 
-    //Edit input values and expected result
-    //const result = await usecase.perform({}, { provider });
-    //expect(result.unwrap()).toEqual();
+  it('performs correctly', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('weather/will-it-rain');
+    const provider = await client.getProvider('openweathermap');
+    const usecase = profile.useCases.Now;
+    const result = await usecase.perform(
+      {
+        coordinates: {
+          latitude: 50.0948541,
+          longitude: 14.4481567,
+        },
+        units: 'metric',
+      },
+      { provider }
+    );
+
+    console.debug(result);
+    expect(result.isOk()).toBeTruthy();
+    expect(result.unwrap().precipitation).toBeDefined();
   });
 });
