6819c06cf4524cab5f0513f2f57118e75e00cf22
Martin Albert <martin.albert.187@gmail.com>
2021-06-16 20:27:07 +0200

    wip: feat: implement ReverseGeocode

--- capabilities/address/geocoding/maps/azure-maps.test.ts
+++ capabilities/address/geocoding/maps/azure-maps.test.ts
@@ -28,5 +28,34 @@ describe('address/geocoding/azure-maps-typed', () => {
     });
   });
 
-  // it('Reverse geocode geographical coordingates', async () => {});
+  it.skip('Reverse geocode geographical coordingates', async () => {
+    const client = new SuperfaceClient();
+    const profile = await client.getProfile('address/geocoding');
+    const provider = await client.getProvider('azure-maps');
+    const usecase = profile.useCases.ReverseGeocode;
+
+    expect(provider).not.toBeUndefined();
+    expect(usecase).not.toBeUndefined();
+    
+    const result = await usecase.perform(
+      {
+        latitude: 50.0593325,
+        longitude: 14.1854451,
+      },
+      { provider }
+    );
+
+    expect(result.isOk()).toBeTruthy();
+    expect(result.unwrap()).toEqual([
+      {
+        addressCountry: 'US',
+        streetAddress: '281 Bedford Avenue',
+        postalCode: '11211',
+        addressRegion: 'NY',
+        addressLocality: 'New York',
+        formattedAddress:
+          '281 Bedford Avenue, New York, NY 11211, United States of America',
+      },
+    ]);
+  });
 });
