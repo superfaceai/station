29a51e36707386c1814e887916fe087cf20a68aa
Ondřej Musil <o@superface.ai>
2021-06-18 15:15:04 +0200

    fix: mock provider

--- capabilities/communication/email-templates/maps/mock.test.ts
+++ capabilities/communication/email-templates/maps/mock.test.ts
@@ -1,21 +1,3 @@
-import { SuperfaceClient } from '../../../../superface/sdk';
+import { emailTemplatestTest } from './email-templates';
 
-describe('communication/email-templates/mock', () => {
-  beforeAll(() => {
-    jest.setTimeout(10000);
-  });
-
-  it('performs correctly', async () => {
-    const client = new SuperfaceClient();
-    const profile = await client.getProfile('communication/email-templates');
-    const provider = await client.getProvider('mock');
-    const usecase = profile.useCases.EmailTemplates;
-
-    expect(provider).not.toBeUndefined();
-    expect(usecase).not.toBeUndefined();
-
-    //Edit input values and expected result
-    //const result = await usecase.perform({}, { provider });
-    //expect(result.unwrap()).toEqual();
-  });
-});
+emailTemplatestTest('mock');
