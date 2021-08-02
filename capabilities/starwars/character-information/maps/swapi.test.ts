8de6dc00bb966fc3a168bfba98b2929d74971aab
Stephen Mizell <smizell@gmail.com>
2021-08-02 13:22:04 -0500

    POC for testing with nock and shapshots

--- capabilities/starwars/character-information/maps/swapi.test.ts
+++ capabilities/starwars/character-information/maps/swapi.test.ts
@@ -1,4 +1,10 @@
+import * as fs from 'fs';
 import { SuperfaceClient } from '../../../../superface/sdk';
+// import nock from 'nock';
+import { back } from 'nock';
+
+back.fixtures = __dirname + '/nockFixtures';
+back.setMode('record')
 
 describe('starwars/character-information/swapi', () => {
   it('performs correctly', async () => {
@@ -6,21 +12,31 @@ describe('starwars/character-information/swapi', () => {
     const profile = await client.getProfile('starwars/character-information');
     const useCase = profile.useCases.RetrieveCharacterInformation;
     const provider = await client.getProvider('swapi');
+    const inputs = {
+      characterName: 'Luke Skywalker',
+    };
 
-    expect(useCase).not.toBeUndefined();
-    expect(provider).not.toBeUndefined();
-
-    const result = await useCase.perform(
-      {
-        characterName: 'Luke Skywalker',
-      },
-      { provider }
-    );
-
-    expect(result.unwrap()).toEqual({
-      height: '172',
-      weight: '77',
-      yearOfBirth: '19BBY',
-    });
+    await testUseCase({ useCase, provider, inputs });
   });
 });
+
+// We would abstract this away in some function call
+async function testUseCase({ useCase, provider, inputs }: any) {
+  expect(useCase).not.toBeUndefined();
+  expect(provider).not.toBeUndefined();
+
+  let result: any;
+
+  // We delete the existing fitures. There shoudl be a better way to do this.
+  if (process.env.TEST === 'integration') {
+    fs.unlinkSync(__dirname + '/nockFixtures/swapi-fixture.json')
+  }
+
+  // If the file doesn't exist, it creates one. If it does, it uses what exists in the file.
+  const { nockDone } = await back('swapi-fixture.json');
+  result = await useCase.perform(inputs, { provider });
+  nockDone();
+
+  // Instead of checking the output, we take a snapshot
+  expect(result.unwrap()).toMatchSnapshot();
+}
