660b888b7fc17b6024e87476a535d5c2656b461a
Artem Kravchenko <artem.kravchenko@applifting.cz>
2021-09-13 17:46:01 +0200

    chore: remove commented code

--- capabilities/weather/get-weather/maps/wttr-in.test.ts
+++ capabilities/weather/get-weather/maps/wttr-in.test.ts
@@ -1,26 +1,3 @@
-// import { SuperfaceClient } from '../../../../superface/sdk';
-//
-// describe('weather/get-weather/wttr-in-typed', () => {
-//   beforeAll(() => {
-//     jest.setTimeout(10000)
-//   })
-//
-//   it('performs correctly', async () => {
-//     const client = new SuperfaceClient();
-//     const profile = await client.getProfile('weather/get-weather');
-//     const provider = await client.getProvider('wttr-in');
-//     const usecase = profile.useCases.GetWeather;
-//
-//     expect(provider).not.toBeUndefined();
-//     expect(usecase).not.toBeUndefined();
-//
-//     const result = await usecase.perform({city: 'Prague,CZ'}, { provider });
-//     expect(typeof result.unwrap().temperature).toBe('number')
-//     expect(typeof result.unwrap().feels_like).toBe('number')
-//     expect(typeof result.unwrap().description).toBe('string')
-//   });
-// });
-
 import {getWeatherTest} from "./get-weather";
 
 getWeatherTest('wttr-in')
