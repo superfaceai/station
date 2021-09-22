f1427a7a88a8822ba48c284982b1327c6f36c013
Ondřej Musil <o@superface.ai>
2021-09-22 17:50:59 +0200

    test: checkFiles of validate

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -17,6 +17,13 @@ describe('validate', () => {
         provider: 'provider',
       },
     ]);
+    mocked(util.localMaps).mockResolvedValue([]);
+    mocked(util.localProfiles).mockResolvedValue([]);
+    mocked(util.localProviders).mockResolvedValue([]);
+    mocked(util.mapsFiles).mockReturnValue([]);
+    mocked(util.profilesFiles).mockReturnValue([]);
+    mocked(util.providersFiles).mockReturnValue([]);
+    mocked(util.arrayDiff).mockReturnValue([]);
   });
 
   afterEach(() => {
@@ -63,6 +70,59 @@ describe('validate', () => {
     });
   });
 
+  describe('checkFiles', () => {
+    it('should get all local files', async () => {
+      await validate.checkFiles();
+
+      expect(util.localProviders).toBeCalled();
+      expect(util.localProfiles).toBeCalled();
+      expect(util.localMaps).toBeCalled();
+    });
+
+    it('should get all files from super.json', async () => {
+      await validate.checkFiles();
+
+      expect(util.providersFiles).toBeCalled();
+      expect(util.profilesFiles).toBeCalled();
+      expect(util.mapsFiles).toBeCalled();
+    });
+
+    it('should call arrayDiff twice', async () => {
+      await validate.checkFiles();
+
+      expect(util.arrayDiff).toBeCalledTimes(2);
+    });
+
+    it('should return not linked files as warning', async () => {
+      mocked(util.arrayDiff)
+        .mockReset()
+        .mockReturnValueOnce(['./provider.json'])
+        .mockReturnValueOnce([]);
+
+      const result = await validate.checkFiles();
+
+      expect(result).toEqual([
+        { kind: 'warn', message: `./provider.json isn't linked in super.json` },
+      ]);
+    });
+
+    it('should return missing files as error', async () => {
+      mocked(util.arrayDiff)
+        .mockReset()
+        .mockReturnValueOnce([])
+        .mockReturnValueOnce(['./provider.json']);
+
+      const result = await validate.checkFiles();
+
+      expect(result).toEqual([
+        {
+          kind: 'error',
+          message: `./provider.json linked in super.json doesn't exists`,
+        },
+      ]);
+    });
+  });
+
   describe('run', () => {
     let print: jest.Mock;
 
@@ -74,6 +134,12 @@ describe('validate', () => {
       await validate.run(print);
 
       expect(check).toBeCalled();
+    });
+
+    it('should call checkFiles', async () => {
+      await validate.run(print);
+
+      expect(util.arrayDiff).toBeCalled();
     });
 
     it('should print No issues for passing validate', async () => {
