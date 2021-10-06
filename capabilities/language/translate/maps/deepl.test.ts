9dade7d90b52a59c96a88a355096d412b589cead
Martin Albert <martin.albert.187@gmail.com>
2021-10-06 11:12:41 +0200

    fix: handle api key from provider security schemes

--- capabilities/language/translate/maps/deepl.test.ts
+++ capabilities/language/translate/maps/deepl.test.ts
@@ -1,32 +1,20 @@
-import { RecordingDefinitions, SuperfaceTest } from '@superfaceai/testing-lib';
+import { SuperfaceTest } from '@superfaceai/testing-lib';
 
 const superface = new SuperfaceTest();
-
-const transformRecording = (recordings: RecordingDefinitions) => {
-  for (const recording of recordings) {
-    recording.path = recording.path.replace(
-      /auth_key=.{41}/g,
-      'auth_key={{credentials removed to keep them secure}}'
-    );
-  }
-};
 
 describe('DeepL', () => {
   describe('TranslateText', () => {
     it('performs correctly', async () => {
       await expect(
-        superface.run(
-          {
-            profile: 'language/translate',
-            provider: 'deepl',
-            useCase: 'TranslateText',
-            input: {
-              text: 'Testovanie',
-              targetLanguage: 'EN',
-            },
+        superface.run({
+          profile: 'language/translate',
+          provider: 'deepl',
+          useCase: 'TranslateText',
+          input: {
+            text: 'Testovanie',
+            targetLanguage: 'EN',
           },
-          { after: transformRecording }
-        )
+        })
       ).resolves.toMatchSnapshot();
     });
   });
