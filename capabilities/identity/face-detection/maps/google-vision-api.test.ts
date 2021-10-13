9e67c514f5321a7e6e09ab973ca5d8f7bbd2dbe2
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-10-13 09:33:41 +0200

    feat: recordings

--- capabilities/identity/face-detection/maps/google-vision-api.test.ts
+++ capabilities/identity/face-detection/maps/google-vision-api.test.ts
@@ -1,4 +1,4 @@
-import { SuperfaceTest } from '@superfaceai/testing-lib';
+import { RecordingScopes, SuperfaceTest } from '@superfaceai/testing-lib';
 
 describe(`identity/face-detection/google-vision-api`, () => {
   let superface: SuperfaceTest;
@@ -18,8 +18,33 @@ describe(`identity/face-detection/google-vision-api`, () => {
             imageUrl:
               'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg',
           },
-        })
+        }, { afterRecordingLoad })
+      ).resolves.toMatchSnapshot();
+    });
+    it('should map error correctly', async () => {
+      await expect(
+        superface.run({
+          profile: 'identity/face-detection',
+          provider: 'google-vision-api',
+          useCase: 'FaceDetection',
+          input: {
+            imageUrl:
+              'https://upload.wikimedia.org',
+          },
+        }, { afterRecordingLoad })
       ).resolves.toMatchSnapshot();
     });
   });
 });
+//FIX: nock is trying to compare actual auth key value (loaded thru env.capabilities) and sanitized value "credentials-removed-to-keep-them-secure"
+//so we are overriding actual value when we are not using live api
+function afterRecordingLoad(scopes: RecordingScopes) {
+  //If we are using recordings mock auth key
+  if (process.env.SUPERFACE_LIVE_API === undefined)
+    scopes.forEach(scope => {
+      scope.filteringPath(
+        /key=[^&]*/g,
+        'key=credentials-removed-to-keep-them-secure'
+      );
+    });
+}
