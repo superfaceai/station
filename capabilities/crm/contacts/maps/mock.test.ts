939fda0343f71a48d6414a43951614f8da3fc37d
Ondřej Musil <o@superface.ai>
2021-10-25 14:56:26 +0200

    chore: add update and search usecases

--- capabilities/crm/contacts/maps/mock.test.ts
+++ capabilities/crm/contacts/maps/mock.test.ts
@@ -14,9 +14,56 @@ describe(`crm/contacts/mock`, () => {
 
       const result = await profile
         .getUseCase('Create')
-        .perform({ email: 'test@example.com' }, { provider });
+        .perform({ email: 'john@example.com' }, { provider });
 
-      expect(result.unwrap()).toEqual({ id: 'mocked@example.com' });
+      expect(result.unwrap()).toEqual({ id: 'john@example.com' });
+    });
+  });
+
+  describe('Update', () => {
+    it('should perform successfully', async () => {
+      const profile = await superface.getProfile('crm/contacts');
+      const provider = await superface.getProvider('mock');
+
+      const result = await profile
+        .getUseCase('Update')
+        .perform(
+          { id: 'john.old@example.com', email: 'john@example.com' },
+          { provider }
+        );
+
+      expect(result.unwrap()).toEqual({ id: 'john@example.com' });
+    });
+  });
+
+  describe('Search', () => {
+    it('should perform successfully', async () => {
+      const profile = await superface.getProfile('crm/contacts');
+      const provider = await superface.getProvider('mock');
+
+      const result = await profile
+        .getUseCase('Search')
+        .perform(
+          { property: 'company', operator: '=', value: 'Example' },
+          { provider }
+        );
+
+      const contacts = result.unwrap() as any[];
+
+      expect(Array.isArray(contacts)).toBeTruthy();
+      expect(contacts).toHaveLength(2);
+      expect(contacts[0]).toEqual({
+        id: 'john@example.com',
+        email: 'john@example.com',
+        phone: '+420 123 456 789',
+        firstName: 'John',
+        lastName: 'Doe',
+        company: 'Example',
+        country: 'Czech republic',
+        customProperties: {
+          waitlist: true,
+        },
+      });
     });
   });
 });
