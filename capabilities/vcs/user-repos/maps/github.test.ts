65e7b04252ca49a5b298f7ff64f3721290c6dbe1
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-10-29 07:46:31 +0200

    chore: use fixed sdk

--- capabilities/vcs/user-repos/maps/github.test.ts
+++ capabilities/vcs/user-repos/maps/github.test.ts
@@ -20,6 +20,18 @@ describe(`scope/name/provider_name}`, () => {
         })
       ).resolves.toMatchSnapshot();
     });
+    it('should perform successfully - user with big number of repos', async () => {
+      await expect(
+        superface.run({
+          profile: 'vcs/user-repos',
+          provider: 'github',
+          useCase: 'UserRepos',
+          input: {
+            user: 'oclif',
+          },
+        })
+      ).resolves.toMatchSnapshot();
+    });
     it('should map error successfully', async () => {
       await expect(
         superface.run({
