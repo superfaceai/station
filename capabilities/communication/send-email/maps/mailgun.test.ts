488f69e1a46512b9597f87b5fb3446f9bfaa2237
Ondřej Musil <o@superface.ai>
2021-11-02 13:49:41 +0100

    feat: mailgun domain as integration parameter

--- capabilities/communication/send-email/maps/mailgun.test.ts
+++ capabilities/communication/send-email/maps/mailgun.test.ts
@@ -1,7 +1,6 @@
 import { sendEmailTest } from './send-email';
 
 sendEmailTest('mailgun', {
-  from: 'hello@demo.superface.org',
-  to: 'hello@superface.ai',
-  domain: 'demo.superface.org',
+  from: 'demo@demo.superface.org',
+  to: 'demo@superface.ai',
 });
