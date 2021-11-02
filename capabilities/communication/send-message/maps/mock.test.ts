30896d7e34e8804283c875808a2df4ff6ffb14df
Ondřej Musil <o@superface.ai>
2021-11-02 17:58:44 +0100

    test: use SuperfaceTest

--- capabilities/communication/send-message/maps/mock.test.ts
+++ capabilities/communication/send-message/maps/mock.test.ts
@@ -1,28 +1,5 @@
-import { SuperfaceClient } from '../../../../superface/sdk';
+import { sendMessage } from './send-message';
 
-describe('communication/send-message', () => {
-  describe('SendMessage', () => {
-    it('performs correctly', async () => {
-      const client = new SuperfaceClient();
-      const profile = await client.getProfile('communication/send-message');
-      const provider = await client.getProvider('mock');
-      const usecase = profile.useCases.SendMessage;
-
-      expect(provider).not.toBeUndefined();
-      expect(usecase).not.toBeUndefined();
-
-      const result = await usecase.perform(
-        {
-          destination: 'my-channel',
-          text: 'Hello from Station',
-        },
-        { provider }
-      );
-
-      expect(result.unwrap()).toEqual({
-        destinationId: 'C1MOCKED',
-        messageId: '1503435956.000247',
-      });
-    });
-  });
+sendMessage('mock', {
+  destination: 'mocked',
 });
--- capabilities/communication/send-message/maps/slack.test.ts
+++ capabilities/communication/send-message/maps/slack.test.ts
@@ -1,30 +1,5 @@
-import { SuperfaceClient } from '../../../../superface/sdk';
+import { sendMessage } from './send-message';
 
-describe('communication/send-message/slack', () => {
-  beforeAll(() => {
-    jest.setTimeout(10000);
-  });
-
-  it('performs correctly', async () => {
-    const client = new SuperfaceClient();
-    const profile = await client.getProfile('communication/send-message');
-    const provider = await client.getProvider('slack');
-    const usecase = profile.useCases.SendMessage;
-
-    expect(provider).not.toBeUndefined();
-    expect(usecase).not.toBeUndefined();
-
-    const result = await usecase.perform(
-      {
-        destination: process.env.SLACK_DESTINATION as string,
-        text: `Station test ${new Date().toISOString()}`,
-      },
-      { provider }
-    );
-
-    const data = result.unwrap();
-
-    expect(data.destinationId).toMatch(/c.+/i);
-    expect(data.messageId).toMatch(/\d+\.\d+/);
-  });
+sendMessage('slack', {
+  destination: 'C0247EMPJNP',
 });
