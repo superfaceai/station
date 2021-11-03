c31435de0e213befee10af04d2989c286381e959
Ondřej Musil <o@superface.ai>
2021-11-03 12:32:54 +0100

    fix: handle mailgun domain in recording path

--- capabilities/communication/send-email/maps/mailgun.test.ts
+++ capabilities/communication/send-email/maps/mailgun.test.ts
@@ -1,6 +1,27 @@
 import { sendEmailTest } from './send-email';
 
-sendEmailTest('mailgun', {
-  from: 'demo@demo.superface.org',
-  to: 'demo@superface.ai',
-});
+sendEmailTest(
+  'mailgun',
+  {
+    from: 'demo@demo.superface.org',
+    to: 'demo@superface.ai',
+  },
+  {
+    beforeRecordingSave: recordings => {
+      recordings.forEach(recording => {
+        recording.path = recording.path.replace(
+          process.env.MAILGUN_DOMAIN as string,
+          'example.com'
+        );
+      });
+    },
+    afterRecordingLoad: scopes => {
+      scopes.forEach(scope => {
+        scope.filteringPath(
+          new RegExp(process.env.MAILGUN_DOMAIN as string, 'g'),
+          'example.com'
+        );
+      });
+    },
+  }
+);
--- capabilities/communication/send-templated-email/maps/mailgun.test.ts
+++ capabilities/communication/send-templated-email/maps/mailgun.test.ts
@@ -1,7 +1,28 @@
 import { sendTemplatedEmailTest } from './send-templated-email';
 
-sendTemplatedEmailTest('mailgun', {
-  from: 'demo@demo.superface.org',
-  to: 'demo@superface.ai',
-  templateId: 'station-test',
-});
+sendTemplatedEmailTest(
+  'mailgun',
+  {
+    from: 'demo@demo.superface.org',
+    to: 'demo@superface.ai',
+    templateId: 'station-test',
+  },
+  {
+    beforeRecordingSave: recordings => {
+      recordings.forEach(recording => {
+        recording.path = recording.path.replace(
+          process.env.MAILGUN_DOMAIN as string,
+          'example.com'
+        );
+      });
+    },
+    afterRecordingLoad: scopes => {
+      scopes.forEach(scope => {
+        scope.filteringPath(
+          new RegExp(process.env.MAILGUN_DOMAIN as string, 'g'),
+          'example.com'
+        );
+      });
+    },
+  }
+);
