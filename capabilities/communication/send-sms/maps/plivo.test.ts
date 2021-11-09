4fc1d7decd56de9dc86d246ecb6eee2f6bde15eb
Martin Albert <martin.albert.187@gmail.com>
2021-11-09 13:22:04 +0100

    fix: hook for plivo send sms tests

--- capabilities/communication/send-sms/maps/plivo.test.ts
+++ capabilities/communication/send-sms/maps/plivo.test.ts
@@ -1,4 +1,38 @@
-import { SuperfaceTest } from '@superfaceai/testing';
+import {
+  RecordingDefinition,
+  RecordingDefinitions,
+  SuperfaceTest,
+} from '@superfaceai/testing';
+
+/* 
+ * TODO: move this to testing library
+   simple parsing is done before load where headers are not matching
+   nock interface nock.Definition
+ */
+function assertRawHeaders(
+  definition: RecordingDefinition
+): asserts definition is RecordingDefinition & { rawHeaders: string[] } {
+  if (!('rawHeaders' in definition)) {
+    throw new Error('rawHeaders not found');
+  }
+}
+
+const beforeRecordingLoad = (definitions: RecordingDefinitions) => {
+  definitions.forEach((def: RecordingDefinition) => {
+    assertRawHeaders(def);
+
+    for (const [name, value] of def.rawHeaders.entries()) {
+      def.rawHeaders[name] = value
+        .toString()
+        .replace(
+          'parameters-removed-to-keep-them-secure',
+          process.env.PLIVO_AUTH_ID as any
+        );
+
+      console.log(name, def.rawHeaders[name]);
+    }
+  });
+};
 
 describe('communication/send-sms/plivo', () => {
   let superface: SuperfaceTest;
@@ -39,12 +73,17 @@ describe('communication/send-sms/plivo', () => {
       const messageId = (result.unwrap() as any).messageId;
 
       await expect(
-        superface.run({
-          useCase: 'RetrieveMessageStatus',
-          input: {
-            messageId,
+        superface.run(
+          {
+            useCase: 'RetrieveMessageStatus',
+            input: {
+              messageId,
+            },
           },
-        })
+          {
+            beforeRecordingLoad,
+          }
+        )
       ).resolves.toMatchSnapshot();
     });
   });
