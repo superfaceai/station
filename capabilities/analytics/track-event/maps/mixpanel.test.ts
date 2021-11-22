ca1e7b644ab4ba8022ce9aa4de2c6f10deaa4168
Ondřej Musil <o@superface.ai>
2021-11-22 11:05:05 +0100

    --wip-- [skip ci]

--- capabilities/analytics/track-event/maps/mixpanel.test.ts
+++ capabilities/analytics/track-event/maps/mixpanel.test.ts
@@ -1,4 +1,18 @@
-import { SuperfaceTest } from '@superfaceai/testing';
+import {
+  RecordingDefinition,
+  RecordingDefinitions,
+  SuperfaceTest,
+} from '@superfaceai/testing';
+
+const beforeRecordingLoad = (definitions: RecordingDefinitions) => {
+  definitions.forEach((def: RecordingDefinition) => {
+    console.log(def.scope);
+    def.scope = def.scope.replace(
+      'parameters-removed-to-keep-them-secure',
+      process.env.MIXPANEL_SERVER as string
+    );
+  });
+};
 
 describe(`analytics/track-event/mixpanel`, () => {
   let superface: SuperfaceTest;
@@ -13,15 +27,18 @@ describe(`analytics/track-event/mixpanel`, () => {
   describe('TrackEvent', () => {
     it('should perform successfully', async () => {
       await expect(
-        superface.run({
-          useCase: 'TrackEvent',
-          input: {
-            eventName: 'stationtest',
-            eventProperties: {
-              user: 'user_1',
+        superface.run(
+          {
+            useCase: 'TrackEvent',
+            input: {
+              eventName: 'stationtest',
+              eventProperties: {
+                user: 'user_1',
+              },
             },
           },
-        })
+          { beforeRecordingLoad }
+        )
       ).resolves.toMatchSnapshot();
     });
   });
