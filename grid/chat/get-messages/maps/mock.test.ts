28e48fd7a8e6080610e1c5655a858358cf914004
Martin Albert <martin.albert.187@gmail.com>
2022-02-03 19:26:18 +0100

    chore: format

--- grid/chat/get-messages/maps/mock.test.ts
+++ grid/chat/get-messages/maps/mock.test.ts
@@ -1,20 +1,17 @@
 import { SuperfaceClient } from '@superfaceai/one-sdk';
 
-describe('chat/get-messages/mock', () => {
-  describe('GetMessages', () => {
+describe('chat/get-destinations/mock', () => {
+  describe('GetDestinations', () => {
     it('performs correctly', async () => {
       const client = new SuperfaceClient();
-      const profile = await client.getProfile('chat/get-messages');
+      const profile = await client.getProfile('chat/get-destinations');
       const provider = await client.getProvider('mock');
-      const usecase = profile.getUseCase('GetMessages');
+      const usecase = profile.getUseCase('GetDestinations');
 
       expect(provider).not.toBeUndefined();
       expect(usecase).not.toBeUndefined();
 
-      const result = await usecase.perform(
-        { destination: 'random' },
-        { provider }
-      );
+      const result = await usecase.perform({}, { provider });
 
       expect((result.unwrap() as any).messages.length).toEqual(3);
     });
