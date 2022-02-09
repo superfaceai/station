df3fa888081f72ee66bc6fa339888050b694d86b
Martin Albert <martin.albert.187@gmail.com>
2022-02-09 11:54:08 +0100

    test(chat/threads): add tests

---
+++ grid/chat/threads/maps/mock.test.ts
@@ -0,0 +1,19 @@
+import { SuperfaceClient } from '@superfaceai/one-sdk';
+
+describe('chat/threads/mock', () => {
+  describe('GetThreads', () => {
+    it('performs correctly', async () => {
+      const client = new SuperfaceClient();
+      const profile = await client.getProfile('chat/threads');
+      const provider = await client.getProvider('mock');
+      const usecase = profile.getUseCase('GetThreads');
+
+      expect(provider).not.toBeUndefined();
+      expect(usecase).not.toBeUndefined();
+
+      const result = await usecase.perform({ server: 'server_id' }, { provider });
+
+      expect(result.isOk() && (result.value as any).threads.length).toEqual(3);
+    });
+  });
+});
---
+++ grid/chat/threads/maps/threads.ts
@@ -0,0 +1,36 @@
+/* eslint-disable jest/no-export */
+
+import { RecordingProcessOptions, SuperfaceTest } from '@superfaceai/testing';
+
+export const getThreadsTest = (
+  provider: string,
+  server: string,
+  options?: RecordingProcessOptions
+): void => {
+  describe(`chat/threads/${provider}`, () => {
+    let superface: SuperfaceTest;
+
+    describe('GetThreads', () => {
+      beforeAll(() => {
+        superface = new SuperfaceTest({
+          profile: 'chat/threads',
+          provider,
+          useCase: 'GetThreads',
+        });
+      });
+
+      it('performs correctly', async () => {
+        await expect(
+          superface.run(
+            {
+              input: {
+                server,
+              },
+            },
+            options
+          )
+        ).resolves.toMatchSnapshot();
+      });
+    });
+  });
+};
