2058b582b571cb2f473317af471fc49f165c82ba
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-02-24 11:53:26 +0100

    fix: Pagination

--- grid/social-media/posts/maps/linkedin.test.ts
+++ grid/social-media/posts/maps/linkedin.test.ts
@@ -7,6 +7,11 @@ const profileId = 'urn:li:organization:2414183';
 describe(`social-media/posts/${provider}`, () => {
   let superfacePosts: SuperfaceTest;
 
+  beforeAll(() => {
+    // LinkedIn API tends to be sluggish
+    jest.setTimeout(20 * 1000);
+  });
+
   beforeEach(() => {
     superfacePosts = new SuperfaceTest({
       profile: 'social-media/posts',
@@ -28,7 +33,7 @@ describe(`social-media/posts/${provider}`, () => {
       });
 
       it('correctly paginates', async () => {
-        for (let i = 0, page = null; i < 2; i++) {
+        for (let i = 0, page = null, firstId = null; i < 3; i++) {
           const result = await superfacePosts.run({
             useCase: 'GetProfilePosts',
             input: {
@@ -38,8 +43,21 @@ describe(`social-media/posts/${provider}`, () => {
           });
           expect(result.isOk()).toBe(true);
           const resultUnwrapped = result.unwrap() as any;
+          // nextPage
+          expect(resultUnwrapped.nextPage).toBeTruthy();
           page = resultUnwrapped.nextPage;
-          expect(page).toBeTruthy();
+          // previous page
+          if (i === 0) {
+            expect(resultUnwrapped.previousPage).toBeUndefined();
+          } else {
+            expect(resultUnwrapped.previousPage).toBeTruthy();
+          }
+          // posts sanity check
+          expect(resultUnwrapped.posts.length).toBeGreaterThan(0);
+          // check that we are not cycling on the same page
+          const newFirstId = resultUnwrapped.posts[0].id;
+          expect(newFirstId).not.toEqual(firstId);
+          firstId = newFirstId;
         }
       });
     });
