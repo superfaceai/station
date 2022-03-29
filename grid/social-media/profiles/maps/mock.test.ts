ed07d66752078ca9176b2d093d2f39892debcdd8
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-03-29 17:29:55 +0200

    feat(social-media): Add profiles use-case

---
+++ grid/social-media/profiles/maps/mock.test.ts
@@ -0,0 +1,3 @@
+import { profilesTest } from './profiles';
+
+profilesTest('mock', { profileIds: ['1232783237623119872'] });
---
+++ grid/social-media/profiles/maps/twitter.test.ts
@@ -0,0 +1,12 @@
+import { profilesTest } from './profiles';
+
+const profileIds = [
+  '12', // jack
+  '783214', // twitter
+  '13334762', // github
+  '1196797704015400960', // superfaceai
+  '1232783237623119872', // NASAPersevere
+  '732521058507620356', // Ukraine
+];
+
+profilesTest('twitter', { profileIds });
