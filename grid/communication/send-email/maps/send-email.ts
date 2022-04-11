511e73034bde6c819d55233039bc617120b14987
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-04-11 16:22:26 +0200

    fix: Set Mailgun domain in .env.example
    It looks like testing library fails to match Mailgun's request
    possibly due to change in path.
    Since the from addresses are hardcoded in tests anyway, there's not
    much use to obfuscate it in .env.

--- grid/communication/send-email/maps/send-email.ts
+++ grid/communication/send-email/maps/send-email.ts
@@ -42,21 +42,31 @@ export const sendEmailTest = (
             )
           ).resolves.toMatchSnapshot();
         });
-      });
 
-      describe('when inputs are invalid', () => {
-        it('should throw an exception', async () => {
+        it('sends email with attachments', async () => {
           const input = {
             ...params,
-            to: 'invalidemail',
-            from: 'invalidemail',
-            subject: '',
-            text: '',
+            subject: 'Station test with attachements',
+            text: `Station test - ${provider}`,
+            attachments: [
+              {
+                content: 'dGVzdA==',
+                type: 'text/plain',
+                filename: 'test.txt',
+              },
+              {
+                content: 'dGVzdC==',
+                type: 'application/json',
+                filename: 'test2.json',
+              },
+            ],
           };
 
           await expect(
             superface.run(
               {
+                profile: 'communication/send-email',
+                provider,
                 useCase: 'SendEmail',
                 input,
               },
@@ -65,31 +75,21 @@ export const sendEmailTest = (
           ).resolves.toMatchSnapshot();
         });
       });
+    });
 
-      it('sends email with attachements', async () => {
+    describe('when inputs are invalid', () => {
+      it('should throw an exception', async () => {
         const input = {
           ...params,
-          subject: 'Station test with attachements',
-          text: `Station test - ${provider}`,
-          attachments: [
-            {
-              content: 'dGVzdA==',
-              type: 'text/plain',
-              filename: 'test.txt',
-            },
-            {
-              content: 'dGVzdC==',
-              type: 'application/json',
-              filename: 'test2.json',
-            },
-          ],
+          to: 'invalidemail',
+          from: 'invalidemail',
+          subject: '',
+          text: '',
         };
 
         await expect(
           superface.run(
             {
-              profile: 'communication/send-email',
-              provider,
               useCase: 'SendEmail',
               input,
             },
