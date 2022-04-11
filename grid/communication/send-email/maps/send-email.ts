e3a71778c63273fbcbf040ad8e6f3c266e2786c5
Jan Vlnas <git.noreply@jan.vlnas.cz>
2022-04-11 17:07:51 +0200

    Revert "Merge pull request #176 from superfaceai/feat/send-email-with-attachements"
    This reverts commit 551509b66ce1f76c53a811cf5e595dbae7b5f148, reversing
    changes made to e648c90c078a6b8eacc4d6d8f11e5f8988a0c703.

--- grid/communication/send-email/maps/send-email.ts
+++ grid/communication/send-email/maps/send-email.ts
@@ -42,31 +42,21 @@ export const sendEmailTest = (
             )
           ).resolves.toMatchSnapshot();
         });
+      });
 
-        it('sends email with attachments', async () => {
+      describe('when inputs are invalid', () => {
+        it('should throw an exception', async () => {
           const input = {
             ...params,
-            subject: 'Station test with attachements',
-            text: `Station test - ${provider}`,
-            attachments: [
-              {
-                content: 'dGVzdA==',
-                type: 'text/plain',
-                filename: 'test.txt',
-              },
-              {
-                content: 'dGVzdC==',
-                type: 'application/json',
-                filename: 'test2.json',
-              },
-            ],
+            to: 'invalidemail',
+            from: 'invalidemail',
+            subject: '',
+            text: '',
           };
 
           await expect(
             superface.run(
               {
-                profile: 'communication/send-email',
-                provider,
                 useCase: 'SendEmail',
                 input,
               },
@@ -74,28 +64,6 @@ export const sendEmailTest = (
             )
           ).resolves.toMatchSnapshot();
         });
-      });
-    });
-
-    describe('when inputs are invalid', () => {
-      it('should throw an exception', async () => {
-        const input = {
-          ...params,
-          to: 'invalidemail',
-          from: 'invalidemail',
-          subject: '',
-          text: '',
-        };
-
-        await expect(
-          superface.run(
-            {
-              useCase: 'SendEmail',
-              input,
-            },
-            options
-          )
-        ).resolves.toMatchSnapshot();
       });
     });
   });
