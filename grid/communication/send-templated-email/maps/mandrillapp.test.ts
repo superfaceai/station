1c62b34d4eab82ad3c53cbe9b5280cc6eb6024aa
Jan Vlnas <616767+jnv@users.noreply.github.com>
2022-05-27 14:55:29 +0200

    chore: Rename mailchimp to mandrillapp (#262)

--- grid/communication/send-templated-email/maps/mandrillapp.test.ts
+++ grid/communication/send-templated-email/maps/mandrillapp.test.ts
@@ -1,6 +1,6 @@
 import { sendTemplatedEmailTest } from './send-templated-email';
 
-sendTemplatedEmailTest('mailchimp', {
+sendTemplatedEmailTest('mandrillapp', {
   from: 'hello@superface.org',
   to: 'hello@superface.org',
   templateId: 'station-test',
