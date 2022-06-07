4a325759bb8b053ba776a5732598759e93e7fdd9
Martin Albert <martin.albert.187@gmail.com>
2022-06-07 14:50:51 +0200

    test: update tests for continuous testing with live traffic

--- grid/chat/messages/maps/messages.ts
+++ grid/chat/messages/maps/messages.ts
@@ -34,9 +34,23 @@ export const getMessagesTest = (
           );
 
           expect(page1.isOk).toBeTruthy();
-          expect(page1).toMatchSnapshot();
 
           /* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
+
+          expect(page1).toMatchSnapshot({
+            value: expect.objectContaining({
+              messages: expect.arrayContaining([
+                expect.objectContaining({
+                  id: expect.any(String),
+                  author: expect.objectContaining({
+                    id: expect.any(String),
+                  }),
+                  createdAt: expect.any(Number),
+                }),
+              ]),
+            }),
+          });
+
           const cursor = page1.isOk()
             ? (page1.value as any).nextPage
             : undefined;
@@ -58,22 +72,43 @@ export const getMessagesTest = (
           );
 
           expect(page2.isOk).toBeTruthy();
-          expect(page2).toMatchSnapshot();
+          expect(page2).toMatchSnapshot({
+            value: expect.objectContaining({
+              messages: expect.arrayContaining([
+                expect.objectContaining({
+                  id: expect.any(String),
+                  author: expect.objectContaining({
+                    id: expect.any(String),
+                  }),
+                  createdAt: expect.any(Number),
+                }),
+              ]),
+            }),
+          });
         });
       });
 
       describe('when specified destination does not exist', () => {
         it('returns error', async () => {
-          await expect(
-            superface.run(
-              {
-                input: {
-                  destination: destination[1],
-                },
+          const result = await superface.run(
+            {
+              input: {
+                destination: destination[1],
               },
-              options
-            )
-          ).resolves.toMatchSnapshot();
+            },
+            options
+          );
+
+          expect(result).toMatchSnapshot({
+            error: expect.objectContaining({
+              kind: expect.stringMatching('HTTPError'),
+              message: expect.stringMatching('Expected HTTP error'),
+              properties: expect.objectContaining({
+                title: expect.any(String),
+              }),
+              statusCode: expect.any(Number),
+            }),
+          });
         });
       });
     });
