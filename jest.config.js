0c26b1bc4124cbaebd9a51248d734bdd105cad58
Martin Albert <martin.albert.187@gmail.com>
2022-08-22 15:03:02 +0200

    fix: return root dir to /grid

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: './',
+  rootDir: './grid',
   runner: 'groups',
-  reporters: ['default', './dist/reporter.js']
+  reporters: ['default', '../dist/reporter.js']
 };
