5309954d36c04e71912a2f73546156b60a51163a
Martin Albert <martin.albert.187@gmail.com>
2022-08-29 10:13:30 +0200

    test(chat/channels/slack): update test and its fixtures - without prepare and teardown

--- grid/chat/channels/maps/slack.test.ts
+++ grid/chat/channels/maps/slack.test.ts
@@ -1,3 +1,38 @@
-import { getChannelsTest } from './channels';
+import { SuperfaceTest } from '@superfaceai/testing';
 
-getChannelsTest('slack');
+describe('chat/channels/slack', () => {
+  let superface: SuperfaceTest;
+
+  describe('GetChannels', () => {
+    beforeAll(() => {
+      superface = new SuperfaceTest({
+        profile: 'chat/channels',
+        provider: 'slack',
+        useCase: 'GetChannels',
+        testInstance: expect,
+      });
+    });
+
+    describe('when specified destination does exist', () => {
+      it('performs correctly', async () => {
+        const result = await superface.run({
+          input: {
+            visibility: 'public',
+            limit: 4,
+          },
+        });
+
+        expect(result).toMatchSnapshot({
+          value: expect.arrayContaining([
+            expect.objectContaining({
+              createdAt: expect.any(Number),
+              id: expect.any(String),
+              membersCount: expect.any(Number),
+              name: expect.any(String),
+            }),
+          ]),
+        });
+      });
+    });
+  });
+});
