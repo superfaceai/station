dfa62d818129127a278e4829153fbab711271ad1
Tomáš Mládek <tomas.mladek@superface.ai>
2022-09-07 16:26:53 +0200

    chore: rename upload-assets -> upload-url

--- grid/social-media/upload-url/maps/instagram.test.ts
+++ grid/social-media/upload-url/maps/instagram.test.ts
@@ -1,6 +1,7 @@
-import { publishVideoTest } from './upload-assets';
+import { publishVideoTest } from './upload-url';
 
-publishVideoTest('instagram', {
+publishVideoTest('instagram', 'single video', {
   caption: 'Single video from Superface Station.',
   url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
+  type: 'video',
 });
---
+++ grid/social-media/upload-url/maps/mock.test.ts
@@ -0,0 +1,6 @@
+import { publishVideoTest } from './upload-url';
+
+publishVideoTest('mock', 'single video', {
+  caption: 'Single video from Superface Station.',
+  url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
+});
