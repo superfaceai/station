1391408dce0bec264baeeb1a141921c51990eb35
Tomáš Mládek <tomas.mladek@superface.ai>
2022-09-07 16:38:07 +0200

    chore: add reel test

--- grid/social-media/upload-url/maps/instagram.test.ts
+++ grid/social-media/upload-url/maps/instagram.test.ts
@@ -5,3 +5,10 @@ publishVideoTest('instagram', 'single video', {
   url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
   type: 'video',
 });
+
+publishVideoTest('instagram', 'single reel', {
+  caption: 'Single reel from Superface Station.',
+  url:
+    'https://assets.mixkit.co/videos/download/mixkit-waves-in-the-water-1164.mp4',
+  type: 'reel',
+});
