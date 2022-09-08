bc7303cb75113a4c3ecc448813e69e2f76523661
Tomáš Mládek <tomas.mladek@superface.ai>
2022-09-08 18:29:44 +0200

    fix: move `reels` to `shortForm` arg, update tests

--- grid/social-media/upload-from-url/maps/instagram.test.ts
+++ grid/social-media/upload-from-url/maps/instagram.test.ts
@@ -3,12 +3,13 @@ import { publishVideoTest } from './upload-from-url';
 publishVideoTest('instagram', 'single video', {
   caption: 'Single video from Superface Station.',
   url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
-  type: 'video',
+  mediaType: 'video',
 });
 
 publishVideoTest('instagram', 'single reel', {
   caption: 'Single reel from Superface Station.',
   url:
     'https://assets.mixkit.co/videos/download/mixkit-waves-in-the-water-1164.mp4',
-  type: 'reel',
+  mediaType: 'video',
+  shortForm: true,
 });
--- grid/social-media/upload-from-url/maps/mock.test.ts
+++ grid/social-media/upload-from-url/maps/mock.test.ts
@@ -3,4 +3,5 @@ import { publishVideoTest } from './upload-from-url';
 publishVideoTest('mock', 'single video', {
   caption: 'Single video from Superface Station.',
   url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
+  mediaType: 'video',
 });
