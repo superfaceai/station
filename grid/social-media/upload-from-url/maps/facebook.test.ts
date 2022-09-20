25e50d66232ba2f3dc86a4151d799044ca4f606f
Tomáš Mládek <tomas.mladek@superface.ai>
2022-09-20 23:25:14 +0200

    feat: add video upload support for facebook

---
+++ grid/social-media/upload-from-url/maps/facebook.test.ts
@@ -0,0 +1,17 @@
+import { publishVideoTest } from './upload-from-url';
+
+publishVideoTest('facebook', 'single video', {
+  caption: 'Single video from Superface Station.',
+  url: 'https://download.samplelib.com/mp4/sample-5s.mp4',
+  uploadType: 'video',
+});
+
+/*
+publishVideoTest('facebook', 'single reel', {
+  caption: 'Single reel from Superface Station.',
+  url:
+    'https://assets.mixkit.co/videos/download/mixkit-waves-in-the-water-1164.mp4',
+  uploadType: 'video',
+  shortFormVideo: true,
+});
+*/
