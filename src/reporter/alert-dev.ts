da372e271eeafa56f99a5e1d2191e65e907b8910
Martin Albert <martin.albert.187@gmail.com>
2022-09-27 13:50:22 +0200

    feat: update dev reporter - report only if some changes were found

--- src/reporter/alert-dev.ts
+++ src/reporter/alert-dev.ts
@@ -1,7 +1,16 @@
 import { TestAnalysis, TestReport } from '@superfaceai/testing';
 
-const parseAnalysis = (analysis: TestAnalysis): string =>
-  `${analysis.impact} change at ${analysis.recordingPath}:
+const parseAnalysis = (analysis: TestAnalysis): string | undefined => {
+  const errorsCount =
+    analysis.errors.added.length +
+    analysis.errors.removed.length +
+    analysis.errors.changed.length;
+
+  if (errorsCount === 0) {
+    return undefined;
+  }
+
+  return `${analysis.impact} change at ${analysis.recordingPath}:
 ${analysis.profileId}/${analysis.providerName}/${analysis.useCaseName}
 
 ADDED:
@@ -14,11 +23,14 @@ CHANGED:
 ${analysis.errors.changed.join('\n')}
 
 `;
+};
 
 export const alertDev = async (report: TestReport): Promise<void> => {
   const reports = report.map(parseAnalysis);
 
   for (const report of reports) {
-    console.warn(report);
+    if (report !== undefined) {
+      console.warn(report);
+    }
   }
 };
