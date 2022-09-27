7ac74d328cd371b2399b22010d414a9acdc5d9e6
Martin Albert <martin.albert.187@gmail.com>
2022-09-27 11:08:29 +0200

    chore: revert changes of working example
    chore: revert changes of working example
    
    Revert "test: update tests for continuous testing with live traffic"
    
    This reverts commit 4a325759bb8b053ba776a5732598759e93e7fdd9.
    
    Revert "chore: update snapshots based on updated tests"
    
    This reverts commit 893a2dc87d72689a9a8d9f68c22ba708a52c6972.
    
    Revert "chore: update recordings based on updated tests"
    
    This reverts commit ebc28d25954742b8491e75ba970595460b9c21d2.
    
    Revert "feat(chat/delete-message): add profile and map for slack"
    
    This reverts commit 8671e7f1ac489c4301758f3cb74c08b71f6a1862.
    
    Revert "chore(chat/delete-message): add snaps and recordings"
    
    This reverts commit f092791d28aaedf5a1913aed80c73366e7347cec.
    
    Revert "test(chat/delete-message): add tests for slack"
    
    This reverts commit 75d377468ac14acee8989705078995ad0b61a56d.
    
    chore: revert other things not needed in PR
    
    chore: revert slack test remake
    
    chore: remove old recordings
    
    chore: revert asana param and recordings

---
+++ grid/chat/messages/maps/messages.ts
@@ -0,0 +1,81 @@
+/* eslint-disable jest/no-export */
+
+import { RecordingProcessOptions, SuperfaceTest } from '@superfaceai/testing';
+
+export const getMessagesTest = (
+  provider: string,
+  destination: string[],
+  options?: RecordingProcessOptions
+): void => {
+  describe(`chat/messages/${provider}`, () => {
+    let superface: SuperfaceTest;
+
+    describe('GetMessages', () => {
+      beforeAll(() => {
+        superface = new SuperfaceTest({
+          profile: 'chat/messages',
+          provider,
+          useCase: 'GetMessages',
+          testInstance: expect,
+        });
+      });
+
+      describe('when specified destination does exist', () => {
+        it('performs correctly', async () => {
+          const page1 = await superface.run(
+            {
+              input: {
+                destination: destination[0],
+                limit: 3,
+              },
+              testName: 'page 1',
+            },
+            options
+          );
+
+          expect(page1.isOk).toBeTruthy();
+          expect(page1).toMatchSnapshot();
+
+          /* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment */
+          const cursor = page1.isOk()
+            ? (page1.value as any).nextPage
+            : undefined;
+
+          if (!cursor) {
+            return;
+          }
+
+          const page2 = await superface.run(
+            {
+              input: {
+                destination: destination[0],
+                limit: 3,
+                page: cursor,
+              },
+              testName: 'page 2',
+            },
+            options
+          );
+
+          expect(page2.isOk).toBeTruthy();
+          expect(page2).toMatchSnapshot();
+        });
+      });
+
+      describe('when specified destination does not exist', () => {
+        it('returns error', async () => {
+          await expect(
+            superface.run(
+              {
+                input: {
+                  destination: destination[1],
+                },
+              },
+              options
+            )
+          ).resolves.toMatchSnapshot();
+        });
+      });
+    });
+  });
+};
