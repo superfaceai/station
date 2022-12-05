535055394d30a2c3e7914ea60815b8504e4a91b7
Jan Halama <mail@janhalama.cz>
2022-12-05 16:42:17 +0100

    feat: add recruitment/jobs profile and mock map

---
+++ grid/recruitment/jobs/maps/mock.test.ts
@@ -0,0 +1,25 @@
+import { SuperfaceClient } from '@superfaceai/one-sdk';
+
+describe('recruitment/jobs/mock', () => {
+  describe('ListJobs', () => {
+    it('performs correctly', async () => {
+      const client = new SuperfaceClient();
+      const profile = await client.getProfile('recruitment/jobs');
+      const provider = await client.getProvider('mock');
+      const usecase = profile.getUseCase('ListJobs');
+
+      expect(provider).not.toBeUndefined();
+      expect(usecase).not.toBeUndefined();
+
+      const result = await usecase.perform({}, { provider });
+
+      expect(result.isOk() && (result.value as any)).toEqual({
+        jobs: [{
+          id: 'JOB_ID',
+          name: 'Software engineer',
+          description: 'We are looking for a Software Engineer who will assist us with design, development and installation of software solutions. Your duties will include development, writing code, and documenting functionality.'
+        }]
+      });
+    });
+  });
+});
