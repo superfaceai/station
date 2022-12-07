ece6367aaeb03bbf14adad94ad97fdedaf2146af
Jan Halama <mail@janhalama.cz>
2022-12-07 15:59:06 +0100

    feat: ATS job board demo use cases (#292)
    * feat: add workable and breezy-hr providers
    
    * feat(recruitment): add profile and maps
    - add profile with `CreateCandidate` usecase
    - add maps with `CreateCandidate` usecase for
      - provider workable
      - mock provider
    - add new profile to super.json
    
    * test(recruitment): add tests for new usecase
    
    * test(recruitment): add recordings and snapshots
    
    * fix(super.json): add missing client id parameter
    
    * Apply suggestions from code review
    
    Co-authored-by: Jan Halama <mail@janhalama.cz>
    
    * fix(recruitment/candidates): review fixes
    
    * fix(recruitment/candidates): return mapped error
    
    * feat(recruitment/candidates): update tests and map
    - use stage `applied` by default
    - add resume among other properties
    
    * test(recruitment/candidates): update recordings and snapshots
    
    * feat(recruitment/candidates): add breezy-hr to super.json
    
    * feat(recruitment/candidates): add breezy-hr map
    
    * test(recruitment/candidates): add tests for breezy-hr
    
    * test(recruitment/candidates): add recordings for breezy-hr
    
    * fix(recruitment/candidates): update breezy-hr map
    
    * fix(recruitment/candidates): update workable recordings and snaps
    
    * fix(recruitment/candidates): map social links
    
    * test(recruitment/candidates): update snapshots
    
    * fix: create candidate conflict error
    
    * feat: add `recruitment/jobs` profile with `ListJobs` use case and  `mock` map (#299)
    
    * feat: add recruitment/jobs profile and mock map
    
    * chore: fix formating
    
    * docs: add successful and failed profile examples
    
    * fix: apply suggestions from code review
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>
    
    * feat(recruitment): add workable map for ListJobs use case (#300)
    
    * fix: let CheckCandidateByEmail fail if 400 status code received
    
    * feat: add `breezy-hr` map for `ListJobs` use case (#301)
    
    * feat: add Breezy HR map for ListJobs use case
    
    * fix: apply suggestions from code review
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>
    
    * fix: review fixes
    
    * feat: update type of recruitment job fields requirements and benefits (#304)
    
    * feat: remove requirements and benefits empty array from jobs breezy hr map
    
    * feat: add optionaly string content for requirements and benefits
    
    * test: update recruitment/jobs test recordings and snapshots
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>

---
+++ grid/recruitment/candidates/maps/candidates.ts
@@ -0,0 +1,119 @@
+/* eslint-disable jest/no-export */
+
+import { RecordingProcessOptions, SuperfaceTest } from '@superfaceai/testing';
+import { readFileSync } from 'fs';
+
+const sampleCandidate = {
+  name: 'John Doe',
+  firstName: 'John',
+  lastName: 'Doe',
+
+  email: 'john.doe@fakemail.com',
+  phone: '1-859-557-6573',
+  address: '25772 Gustave Shore, Iowa, USA',
+
+  education: [
+    {
+      degree: 'MBA',
+      school: 'University of Pennsylvania',
+      fieldOfStudy: null,
+      startedAt: '2008-03-01',
+      endedAt: '2011-03-30',
+    },
+    {
+      degree: 'B.S.',
+      school: 'University of Chicago',
+      fieldOfStudy: 'Marketing Communication & Economics',
+      startedAt: '2004-09-01',
+      endedAt: '2007-03-30',
+    },
+  ],
+
+  workExperience: [
+    {
+      position: 'Sales Director',
+      summary: 'Summary of a work experience at Test Company',
+      company: 'Test Company',
+      industry: 'Telecommunications',
+      current: false,
+      startedAt: '2011-03-01',
+      endedAt: '2014-03-30',
+    },
+  ],
+
+  cv: {
+    name: 'cv-sample.pdf',
+    data: readFileSync('./grid/recruitment/candidates/cv-sample.pdf', {
+      encoding: 'base64',
+    }),
+  },
+
+  links: [
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/candidate.username',
+    },
+    {
+      name: 'Portfolio',
+      url: 'https://url.to.portfolio',
+    },
+  ],
+};
+
+export const candidatesTest = (
+  provider: string,
+  jobIds: { valid: string; invalid: string },
+  options?: RecordingProcessOptions
+): void => {
+  describe(`recruitment/candidates/${provider}`, () => {
+    let superface: SuperfaceTest;
+
+    describe('CreateCandidate', () => {
+      beforeAll(() => {
+        jest.setTimeout(10000);
+        superface = new SuperfaceTest({
+          profile: 'recruitment/candidates',
+          provider,
+          useCase: 'CreateCandidate',
+          testInstance: expect,
+        });
+      });
+
+      describe('when specified job does exist', () => {
+        it('performs correctly', async () => {
+          const page1 = await superface.run(
+            {
+              input: {
+                jobId: jobIds.valid,
+                ...sampleCandidate,
+              },
+              testName: 'page 1',
+            },
+            options
+          );
+
+          expect(page1.isOk).toBeTruthy();
+          expect(page1).toMatchSnapshot();
+        });
+      });
+
+      describe('when specified job does not exist', () => {
+        it('returns error', async () => {
+          await expect(
+            superface.run(
+              {
+                input: {
+                  jobId: jobIds.invalid,
+                  firstName: 'Demo',
+                  lastName: 'Testing',
+                  email: 'demo_testing@fakemail.com',
+                },
+              },
+              options
+            )
+          ).resolves.toMatchSnapshot();
+        });
+      });
+    });
+  });
+};
