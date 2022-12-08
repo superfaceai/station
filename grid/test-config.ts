bfa4a2ebfe5af9413974aac40477b15654addef0
Martin Albert <martin.albert.187@gmail.com>
2022-12-08 09:37:05 +0100

    chore(test-helpers): correct assert of buildSuperfaceTest

--- grid/test-config.ts
+++ grid/test-config.ts
@@ -1,7 +1,14 @@
-import { NockConfig } from '@superfaceai/testing';
+import {
+  NockConfig,
+  SuperfaceTest,
+  SuperfaceTestConfig,
+} from '@superfaceai/testing';
 
 export const nockConfig: NockConfig = {
   testInstance: expect,
 };
 
-export const buildSuperfaceTest = (global as any).buildSuperfaceTest;
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */
+export const buildSuperfaceTest = (global as any).buildSuperfaceTest as (
+  options?: SuperfaceTestConfig
+) => SuperfaceTest;
