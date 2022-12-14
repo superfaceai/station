cddf5ffb804ae8569f709627ee4bc155d9057534
Martin Albert <martin.albert.187@gmail.com>
2022-12-14 18:17:42 +0100

    chore(recruitment/get-stage-changes): format

--- grid/recruitment/get-stage-changes/maps/workable.test.ts
+++ grid/recruitment/get-stage-changes/maps/workable.test.ts
@@ -1,7 +1,4 @@
 import { getStageChangesTest } from './get-stage-changes';
 
 // TODO: need valid access token for workable (trial ended)
-getStageChangesTest(
-  'workable',
-  { valid: '', invalid: 'NOT-EXISTING' }
-);
+getStageChangesTest('workable', { valid: '', invalid: 'NOT-EXISTING' });
