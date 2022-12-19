9d8e08097ef32f8965d444b43897f3096822272a
Martin Albert <martin.albert.187@gmail.com>
2022-12-19 14:26:06 +0100

    fix(recruitment/get-stage-changes/workable): update map and test

--- grid/recruitment/get-stage-changes/maps/workable.test.ts
+++ grid/recruitment/get-stage-changes/maps/workable.test.ts
@@ -1,3 +1,7 @@
 import { getStageChangesTest } from './get-stage-changes';
 
-getStageChangesTest('workable', { valid: 'd270c55', invalid: 'NOT-EXISTING' });
+getStageChangesTest(
+  'workable',
+  { valid: '2ba8f2' },
+  { valid: 'd270c55', invalid: 'NOT-EXISTING' }
+);
