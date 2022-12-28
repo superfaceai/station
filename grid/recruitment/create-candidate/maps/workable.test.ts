899dcadff539c4241cc9a2136461c5549862c710
Jan Halama <mail@janhalama.cz>
2022-12-28 14:36:20 +0100

    feat(recruitment): add create-candidate 2.0 workable map

--- grid/recruitment/create-candidate/maps/workable.test.ts
+++ grid/recruitment/create-candidate/maps/workable.test.ts
@@ -1,6 +1,11 @@
 import { createCandidateTest } from './create-candidate';
+import { createCandidateFeaturesTest } from './create-candidate-features';
 
-createCandidateTest('workable', {
-  valid: 'FCFA80DF09',
+const WORKABLE_PROVIDER_NAME = 'workable';
+
+createCandidateTest(WORKABLE_PROVIDER_NAME, {
+  valid: '04D2AB5A08',
   invalid: 'NOT-EXISTING',
 });
+
+createCandidateFeaturesTest(WORKABLE_PROVIDER_NAME);
