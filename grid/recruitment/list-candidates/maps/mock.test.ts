cd9815efe9a0d7764b06322b89e734ce37642aee
Jan Halama <mail@janhalama.cz>
2023-01-02 16:00:31 +0100

    feat: add recruitment/list-candidates profile

---
+++ grid/recruitment/list-candidates/maps/mock.test.ts
@@ -0,0 +1,28 @@
+import { SuperfaceTest } from '@superfaceai/testing';
+
+describe('recruitment/list-candidates/mock', () => {
+  let superface: SuperfaceTest;
+
+  beforeEach(() => {
+    superface = new SuperfaceTest({
+      profile: 'recruitment/list-candidates',
+      provider: 'mock'
+    });
+  });
+
+  describe('ListCandidates', () => {
+    
+    // specify test case name
+    it('should perform successfully', async () => {
+      const result = await superface.run({
+        useCase: 'ListCandidates',
+        input: {
+          jobId : 'JOB_ID',
+        }
+      })
+      
+      expect(() => result.unwrap()).not.toThrow();
+      expect(result).toMatchSnapshot();
+    });
+  });  
+});
