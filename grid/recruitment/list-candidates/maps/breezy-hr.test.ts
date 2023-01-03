c714d64ae055b888883297222e48793b5c96ddaf
Jan Halama <mail@janhalama.cz>
2023-01-03 10:27:27 +0100

    feat: add list candidates breezy-hr map

---
+++ grid/recruitment/list-candidates/maps/breezy-hr.test.ts
@@ -0,0 +1,6 @@
+import { listCandidatesTest } from './list-candidates';
+
+listCandidatesTest('breezy-hr', {
+    validJobId: '701c3f2fc42a01',
+    invalidJobId: '12345ab789d1dd'
+});
---
+++ grid/recruitment/list-candidates/maps/list-candidates.ts
@@ -0,0 +1,97 @@
+/* eslint-disable jest/no-export */
+
+import { RecordingProcessOptions, SuperfaceTest } from '@superfaceai/testing';
+
+import { buildSuperfaceTest } from '../../../test-config';
+
+const describeIf = (condition: boolean): jest.Describe =>
+  condition ? describe : describe.skip;
+
+export const listCandidatesTest = (
+  provider: string, 
+  testInputs: {
+    validJobId: string,
+    invalidJobId: string
+  },
+  options?: RecordingProcessOptions
+): void => {
+  describe(`recruitment/list-candidates/${provider}`, () => {
+    let superface: SuperfaceTest;
+
+    describe('ListCandidates', () => {
+      beforeAll(() => {
+        superface = buildSuperfaceTest({
+          profile: 'recruitment/list-candidates',
+          provider,
+          useCase: 'ListCandidates',
+        });
+      });
+
+      describe('when searching for candidates with valid job ID', () => {
+        it('returns list of candidates', async () => {
+          const result = await superface.run(
+            {
+              input: {
+                jobId: testInputs.validJobId,
+              },
+              testName: 'list candidates with valid job ID',
+            },
+            options
+          );
+
+          expect(() => result.unwrap()).not.toThrow();
+          expect(result).toMatchSnapshot();
+        });
+      });
+
+      describe('when searching for candidates with invalid job ID', () => {
+        it('returns list of candidates', async () => {
+          const result = await superface.run(
+            {
+              input: {
+                jobId: testInputs.invalidJobId,
+              },
+              testName: 'list candidates with invalid job ID',
+            },
+            options
+          );
+
+          expect(() => result.unwrap()).toThrow();
+          expect(result).toMatchSnapshot();
+        });
+      });
+
+      // eslint-disable-next-line jest/no-disabled-tests
+      describeIf(provider === 'breezy-hr')(
+        'when specified company does not exist',
+        () => {
+          let companyId: string | undefined;
+
+          beforeAll(() => {
+            companyId = process.env.BREEZY_HR_COMPANY_ID;
+
+            process.env.BREEZY_HR_COMPANY_ID = '1b111c1111ef11';
+          });
+
+          afterAll(() => {
+            process.env.BREEZY_HR_COMPANY_ID = companyId;
+          });
+
+          it('returns error', async () => {
+            const result = await superface.run(
+              {
+                input: {
+                  jobId: 'JOB_ID',
+                },
+              },
+              options
+            );
+
+            expect(() => result.unwrap()).toThrow();
+            expect(result).toMatchSnapshot();
+          });
+        }
+      );
+    });
+  });
+};
