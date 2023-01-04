549d7f3224b230f742d1e7b1755491ce6faf74cf
Jan Halama <mail@janhalama.cz>
2023-01-04 13:34:24 +0100

    feat: add recruitment/create-candidate 2.0 profile (#326)
    * feat(recruitment): add new create-candidate 2.0.0 profile version
    
    * feat(recruitment): create-candidate 2.0 mock map
    
    * feat(recruitment): add create-candidate 2.0 breezy-hr map
    
    * feat(recruitment): add create-candidate 2.0 workable map
    
    * chore: fix linter errors
    
    * chore: fix perttier errors
    
    * chore: remove unnecessary linter directive
    
    * docs: fix recruitment/create-candidate profile docs
    
    * docs: fix typo
    
    * chore: fix formating
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>
    
    * test: fix create-candidate workable test snapshots and recordings
    
    Co-authored-by: Martin Albert <martin.albert.187@gmail.com>

---
+++ grid/recruitment/create-candidate/maps/create-candidate-features.ts
@@ -0,0 +1,35 @@
+/* eslint-disable jest/no-export */
+import { RecordingProcessOptions, SuperfaceTest } from '@superfaceai/testing';
+
+import { buildSuperfaceTest } from '../../../test-config';
+
+export const createCandidateFeaturesTest = (
+  provider: string,
+  options?: RecordingProcessOptions
+): void => {
+  describe(`recruitment/create-candidate/${provider}`, () => {
+    describe('CreateCandidateFeatures', () => {
+      let superface: SuperfaceTest;
+
+      beforeAll(() => {
+        superface = buildSuperfaceTest({
+          profile: 'recruitment/create-candidate',
+          provider,
+          useCase: 'CreateCandidateFeatures',
+        });
+      });
+
+      it(`returns valid ${provider} features`, async () => {
+        const result = await superface.run(
+          {
+            input: {},
+          },
+          options
+        );
+
+        expect(result.isOk()).toBeTruthy();
+        expect(result).toMatchSnapshot();
+      });
+    });
+  });
+};
