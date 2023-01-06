08c759b6ed946b1759545b55f87a9a779047efa0
Jan Halama <mail@janhalama.cz>
2023-01-06 13:18:46 +0100

    feat: add  `recruitment/list-candidates` profile `breezy-hr` map (#329)
    * feat: add list candidates breezy-hr map
    
    * chore: fix linter errors
    
    * test: move breezy-hr specific test to separate test function to avoid skipped tests
    
    * chore: fix formating
    
    * test: fix case name

---
+++ grid/recruitment/list-candidates/maps/breezy-hr.test.ts
@@ -0,0 +1,11 @@
+import {
+  listCandidatesBreezyHRSpecificTest,
+  listCandidatesTest,
+} from './list-candidates';
+
+listCandidatesTest('breezy-hr', {
+  validJobId: '214429f1ca4c01',
+  invalidJobId: '12345ab789d1dd',
+});
+
+listCandidatesBreezyHRSpecificTest();
