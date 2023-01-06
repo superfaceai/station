51f7de90df72b4ef3f2d540b0b883b353018f5ec
Jan Halama <mail@janhalama.cz>
2023-01-06 11:35:53 +0100

    feat: add `recruitment/list-candidates` profile (#328)
    * feat: add recruitment/list-candidates profile
    
    * chore: fix formating
    
    * chore: remove template comment from test
    
    * fix: recruitment/list-candidates profile example
    
    * docs: reference recruitment/list-jobs profile in the docs
    
    * docs: fix description of page cursor

---
+++ grid/recruitment/list-candidates/maps/mock.test.ts
@@ -0,0 +1,26 @@
+import { SuperfaceTest } from '@superfaceai/testing';
+
+describe('recruitment/list-candidates/mock', () => {
+  let superface: SuperfaceTest;
+
+  beforeEach(() => {
+    superface = new SuperfaceTest({
+      profile: 'recruitment/list-candidates',
+      provider: 'mock',
+    });
+  });
+
+  describe('ListCandidates', () => {
+    it('should perform successfully', async () => {
+      const result = await superface.run({
+        useCase: 'ListCandidates',
+        input: {
+          jobId: 'JOB_ID',
+        },
+      });
+
+      expect(() => result.unwrap()).not.toThrow();
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
