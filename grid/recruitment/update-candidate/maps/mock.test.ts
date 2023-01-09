639beefd2cd85e41b323cda70a6c072e84beffb5
Jan Halama <mail@janhalama.cz>
2023-01-09 17:14:12 +0100

    feat: add `recruitment/update-candidate` profile and `mock` map (#335)
    * feat: add recruitment/update-candidate profile and mock map
    
    * feat: add UpdateCandidateFeatures use case into recruitment/update-candidate profile
    
    * fix(recruitment):  add missing models to update-candidate profile
    
    * feat: add CVFileNameRequired and CVMIMETypeNotSupported to update-candidate profile error codes
    
    * fix(recruitment): update-profile docs
    
    * chore: fix indentation
    
    * docs(recruitment): add description of update-candidate input cv fields
    
    * fix(recruitment): update-candidate profile ids
    
    * feat: add SuccessfulCVUpdate example to Updatecnadidate profile

---
+++ grid/recruitment/update-candidate/maps/mock.test.ts
@@ -0,0 +1,42 @@
+import { SuperfaceTest } from '@superfaceai/testing';
+
+import { buildSuperfaceTest } from '../../../test-config';
+
+describe('recruitment/update-candidate/mock', () => {
+  let superface: SuperfaceTest;
+
+  beforeEach(() => {
+    superface = buildSuperfaceTest({
+      profile: 'recruitment/update-candidate',
+      provider: 'mock',
+    });
+  });
+
+  describe('UpdateCandidate', () => {
+    it('should perform successfully', async () => {
+      const result = await superface.run({
+        useCase: 'UpdateCandidate',
+        input: {
+          candidateId: 'CANDIDATE_ID',
+          firstName: 'John',
+          lastName: 'Doe',
+        },
+      });
+
+      expect(() => result.unwrap()).not.toThrow();
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe('UpdateCandidateFeatures', () => {
+    it('should perform successfully', async () => {
+      const result = await superface.run({
+        useCase: 'UpdateCandidateFeatures',
+        input: {},
+      });
+
+      expect(() => result.unwrap()).not.toThrow();
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
