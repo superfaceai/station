b84a4b316bc96bc7c12c850c9ad7479ebaf7a8dc
Jan Halama <mail@janhalama.cz>
2023-01-10 10:05:01 +0100

    feat: add conversion/doc-to-text profile with mock provider

---
+++ grid/conversion/doc-to-text/maps/mock.test.ts
@@ -0,0 +1,31 @@
+import { SuperfaceTest } from '@superfaceai/testing';
+
+import { buildSuperfaceTest } from '../../../test-config';
+
+describe('conversion/doc-to-text/mock', () => {
+  let superface: SuperfaceTest;
+
+  beforeEach(() => {
+    superface = buildSuperfaceTest({
+      profile: 'conversion/doc-to-text',
+      provider: 'mock'
+});
+  });
+
+  describe('DocToText', () => {
+    
+    // specify test case name
+    it('should perform successfully', async () => {
+      const result = await superface.run({
+        useCase: 'ConvertDocumentToText',
+        input: {
+          fileName : 'big-bang.pdf',
+          content : '<BinaryData>',
+        }
+      })
+      
+      expect(() => result.unwrap()).not.toThrow();
+      expect(result).toMatchSnapshot();
+    });
+  });  
+});
