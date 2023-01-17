43ae8255bff31c51771f991b1e4b126ae2aca6aa
Tomáš Mládek <tomas.mladek@superface.ai>
2023-01-17 16:57:57 +0100

    chore: prettier fix

--- grid/crm/lists/maps/sendgrid.test.ts
+++ grid/crm/lists/maps/sendgrid.test.ts
@@ -4,5 +4,5 @@ getListsTest('sendgrid');
 addContactsToListTest(
   'sendgrid',
   ['demo+added_one@superface.ai', 'demo+added_two@superface.ai'],
-  '17a9df8c-9003-4c60-8e27-8492aebdacce',
+  '17a9df8c-9003-4c60-8e27-8492aebdacce'
 );
