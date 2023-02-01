1ee0055252adaa48fcfaeff5f5590bf485ef8b52
Tomáš Mládek <tomas.mladek@superface.ai>
2023-02-01 16:07:12 +0100

    feat: add list use cases for hubspot
    also update hubspot provider due to https://developers.hubspot.com/changelog/upcoming-api-key-sunset

---
+++ grid/crm/add-contacts-to-list/maps/hubspot.test.ts
@@ -0,0 +1,7 @@
+import { addContactsToListTest } from './add-contacts-to-list';
+
+addContactsToListTest(
+  'hubspot',
+  ['emailmaria@hubspot.com', 'bh@hubspot.com'], // HubSpot-provided test e-mails
+  '1'
+);
