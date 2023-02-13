1d903794bdaf420a333ef98007b13cdb9b399c4b
Jan Halama <mail@janhalama.cz>
2023-02-13 11:51:01 +0100

    fix: redact Facebook page access token in recordings of get profile test

--- grid/social-media/posts/maps/facebook.test.ts
+++ grid/social-media/posts/maps/facebook.test.ts
@@ -1,3 +1,31 @@
+import { RecordingDefinitions } from '@superfaceai/testing';
+
 import { getProfilePostsTest } from './posts';
 
-getProfilePostsTest('facebook', 'Sf-test');
+const PAGE_ACCESS_TOKEN_REDACTED_VALUE = 'PAGE_ACCESS_TOKEN_VALUE_REDACTED';
+
+// Remove page access tokens from internal requests and responses
+const beforeRecordingSave = function (recordings: RecordingDefinitions) {
+  let pageAccessToken: string | undefined;
+
+  recordings.forEach(recording => {
+    if (pageAccessToken) {
+      recording.path = recording.path.replace(
+        pageAccessToken,
+        PAGE_ACCESS_TOKEN_REDACTED_VALUE
+      );
+    }
+    if (recording.response) {
+      const response = recording.response as { access_token?: string };
+      if (response.access_token) {
+        pageAccessToken = response.access_token;
+        recording.response = {
+          ...response,
+          access_token: PAGE_ACCESS_TOKEN_REDACTED_VALUE,
+        };
+      }
+    }
+  });
+};
+
+getProfilePostsTest('facebook', 'Sf-test', { beforeRecordingSave });
