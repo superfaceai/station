c3135c336ff8f226a416eafa7cdf7175e1fa010b
Branko Gvoka <bane92ru@gmail.com>
2023-03-02 20:14:39 +0100

    chore: extract logic into ops

--- grid/recruitment/get-cv/maps/greenhouse.test.ts
+++ grid/recruitment/get-cv/maps/greenhouse.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable jest/no-export */
 
-import { getCVTest } from "./get-cv";
+import { getCVTest } from './get-cv';
 
 const testCandidates = {
   notFound: {
@@ -12,14 +12,14 @@ const testCandidates = {
   },
   withoutCV: {
     name: 'John NoCV',
-    candidateId: '7413692006'
-  }
-}
+    candidateId: '7413692006',
+  },
+};
 
 getCVTest('greenhouse', {
   validCandidateId: testCandidates.withCV.candidateId,
   invalidCandidateIds: [
     testCandidates.withoutCV.candidateId,
-    testCandidates.notFound.candidateId
-  ]
-})
+    testCandidates.notFound.candidateId,
+  ],
+});
