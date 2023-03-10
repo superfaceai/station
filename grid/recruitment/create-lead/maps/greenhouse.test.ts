18a63abd34ea17160927cecfb631cb8db1e33195
cigar-galaxy82 <shantanushukla64@gmail.com>
2023-03-10 21:26:23 +0530

    fix: create-lead map and test for greenhouse

--- grid/recruitment/create-lead/maps/greenhouse.test.ts
+++ grid/recruitment/create-lead/maps/greenhouse.test.ts
@@ -1,8 +1,3 @@
 import { createLeadTest } from './create-lead';
-const GREENHOUSE_PROVIDER_NAME = 'greenhouse';
 
-createLeadTest(GREENHOUSE_PROVIDER_NAME, {
-  valid: '4031896006',
-  invalid: '-1',
-});
-
+createLeadTest('greenhouse', { valid: '4031896006', invalid: '-1' });
