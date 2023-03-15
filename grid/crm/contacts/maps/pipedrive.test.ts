4fbc04f2d5465de672ae8cf369a66ed490bce70e
Branko Gvoka <bane92ru@gmail.com>
2023-03-15 11:49:33 +0100

    feat: add crm/contracts pipedrive create use case with org get or create

--- grid/crm/contacts/maps/pipedrive.test.ts
+++ grid/crm/contacts/maps/pipedrive.test.ts
@@ -12,6 +12,23 @@ describe('crm/contracts/pipedrive', () => {
     });
   });
 
+  describe('Create', () => {
+    it('should perform successfully', async () => {
+      await expect(
+        superface.run({
+          useCase: 'Create',
+          input: {
+            email: 'test@bshdo.co',
+            phone: '+111',
+            firstName: 'Test',
+            lastName: 'Gvoka',
+            company: 'Test Bushido'
+          },
+        })
+      ).resolves.toMatchSnapshot();
+    });
+  })
+
   describe('Search', () => {
     it('should perform successfully', async () => {
       await expect(
@@ -25,9 +42,7 @@ describe('crm/contracts/pipedrive', () => {
         })
       ).resolves.toMatchSnapshot();
     });
-  });
 
-  describe('Search', () => {
     it('should perform unsuccessfully', async () => {
       await expect(
         superface.run({
