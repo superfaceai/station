7dfddf63dfed7c5cb385612e83d4ba76c73795bc
Jan Halama <mail@janhalama.cz>
2023-04-26 17:21:01 +0200

    feat(communication): mandrill map for communication/send-email@2.2 profile

--- grid/communication/send-email/maps/mandrillapp.test.ts
+++ grid/communication/send-email/maps/mandrillapp.test.ts
@@ -3,4 +3,5 @@ import { sendEmailTest } from './send-email';
 sendEmailTest('mandrillapp', {
   from: 'hello@demo.superface.org',
   to: 'hello@demo.superface.org',
+  replyTo: 'hello+replyto@demo.superface.org',
 });
