781dce7a2823b329c2b9720f2173b8b28f290bd4
Jan Halama <mail@janhalama.cz>
2023-04-26 17:04:56 +0200

    feat(communication): postmark map for communication/send-email@2.2 profile

--- grid/communication/send-email/maps/postmark.test.ts
+++ grid/communication/send-email/maps/postmark.test.ts
@@ -3,4 +3,5 @@ import { sendEmailTest } from './send-email';
 sendEmailTest('postmark', {
   from: 'demo@demo.superface.org',
   to: 'demo@demo.superface.org',
+  replyTo: 'demo+replyto@demo.superface.org',
 });
