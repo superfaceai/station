f357da19285602e8633f9a557fdefaddd2a21307
Jan Halama <mail@janhalama.cz>
2023-04-26 15:55:43 +0200

    feat(communication): add sendgrid map for communication/send-email@2.2 profile

--- grid/communication/send-email/maps/sendgrid.test.ts
+++ grid/communication/send-email/maps/sendgrid.test.ts
@@ -3,4 +3,5 @@ import { sendEmailTest } from './send-email';
 sendEmailTest('sendgrid', {
   from: 'demo@demo.superface.org',
   to: 'demo@demo.superface.org',
+  replyTo: 'demo+replyto@demo.superface.org',
 });
