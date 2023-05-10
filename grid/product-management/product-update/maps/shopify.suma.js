05c6588cb31e6aaf7f88a582c194659ffac240d6
Jakub Vacek <vacjak@seznam.cz>
2023-05-10 15:01:44 +0200

    feat: update product error handling

--- grid/product-management/product-update/maps/shopify.suma.js
+++ grid/product-management/product-update/maps/shopify.suma.js
@@ -1,4 +1,4 @@
-function UpdateProduct({ input, parameters, services }) {
+function UpdateProduct({ input, services }) {
   const url = `${services.default}/admin/api/2023-04/products/${input.product.id}.json`;
   const options = {
     method: 'PUT',
@@ -14,14 +14,29 @@ function UpdateProduct({ input, parameters, services }) {
   const response = std.unstable.fetch(url, options).response();
   const body = response.bodyAuto() ?? {};
 
-  if (response.status !== 200) {
+  if (response.status >= 400) {
+    let errorMessage = 'An error occurred while retrieving the customer.';
+    if (response.status === 401) {
+      errorMessage = '[API] Invalid API key or access token (unrecognized login or wrong password)';
+    } else if (response.status === 402) {
+      errorMessage = "This shop's plan does not have access to this feature";
+    } else if (response.status === 403) {
+      errorMessage = 'User does not have access';
+    } else if (response.status === 404) {
+      errorMessage = 'Not Found';
+    } else if (response.status === 422) {
+      errorMessage = 'The request body contains semantic errors.';
+    } else if (response.status === 429) {
+      errorMessage = 'Exceeded 2 calls per second for api client. Reduce request rates to resume uninterrupted service.';
+    } else if (response.status >= 500) {
+      errorMessage = 'An unexpected error occurred';
+    }
+
     throw new std.unstable.MapError({
-      errors: [
-        {
-          code: response.status.toString(),
-          message: 'HTTP call failed',
-        },
-      ],
+      errors: errorMessage,
+      status: response.status,
+      retryAfter: response.headers['Retry-After']?.[0] ? Number(response.headers['Retry-After'][0]) : null,
+      xShopifyShopApiCallLimit: response.headers['X-Shopify-Shop-Api-Call-Limit']?.[0] ? response.headers['X-Shopify-Shop-Api-Call-Limit'][0] : null,
     });
   }
 
