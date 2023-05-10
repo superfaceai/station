8fa8f378183c6f83035408c9b671e5d809eb8c55
Jakub Vacek <vacjak@seznam.cz>
2023-05-10 12:42:33 +0200

    fix: review fixes

--- grid/customer-management/create-customer/maps/shopify.suma.js
+++ grid/customer-management/create-customer/maps/shopify.suma.js
@@ -1,8 +1,8 @@
-function CreateCustomer({ input, parameters, services }) {
+function CreateCustomer({ input, services }) {
   const url = `${services.default}/admin/api/2023-04/customers.json`;
   const options = {
     method: 'POST',
-    body: input.customer,
+    body: input,
     headers: {
       'Content-Type': 'application/json',
     },
@@ -13,45 +13,57 @@ function CreateCustomer({ input, parameters, services }) {
   const body = response.bodyAuto() ?? {};
 
   if (response.status !== 201) {
+    let errorMessage = 'An error occurred while creating the customer.';
+    if (response.status === 401) {
+      errorMessage = '[API] Invalid API key or access token (unrecognized login or wrong password)';
+    } else if (response.status === 402) {
+      errorMessage = "This shop's plan does not have access to this feature";
+    } else if (response.status === 403) {
+      errorMessage = 'User does not have access';
+    } else if (response.status === 404) {
+      errorMessage = 'Not Found';
+    } else if (response.status === 422) {
+      errorMessage = 'The request body contains semantic errors.';
+    } else if (response.status === 429) {
+      errorMessage = 'Exceeded 2 calls per second for api client. Reduce request rates to resume uninterrupted service.';
+    } else if (response.status >= 500) {
+      errorMessage = 'An unexpected error occurred';
+    }
+
     throw new std.unstable.MapError({
-      errors: [
-        {
-          message: 'Failed to create customer',
-          code: response.status.toString(),
-        },
-      ],
+      errors: errorMessage,
+      status: response.status,
+      retryAfter: response.headers['Retry-After']?.[0] ? Number(response.headers['Retry-After'][0]) : null,
+      xShopifyShopApiCallLimit: response.headers['X-Shopify-Shop-Api-Call-Limit']?.[0] ? response.headers['X-Shopify-Shop-Api-Call-Limit'][0] : null,
     });
   }
 
   const result = {
     customer: {
-      id: body.customer?.id,
-      email: body.customer?.email,
-      accepts_marketing: body.customer?.accepts_marketing,
-      created_at: body.customer?.created_at,
-      updated_at: body.customer?.updated_at,
-      first_name: body.customer?.first_name,
-      last_name: body.customer?.last_name,
-      orders_count: body.customer?.orders_count,
-      state: body.customer?.state,
-      total_spent: body.customer?.total_spent,
-      last_order_id: body.customer?.last_order_id,
-      note: body.customer?.note,
-      verified_email: body.customer?.verified_email,
-      multipass_identifier: body.customer?.multipass_identifier,
-      tax_exempt: body.customer?.tax_exempt,
-      tags: body.customer?.tags,
-      last_order_name: body.customer?.last_order_name,
-      currency: body.customer?.currency,
-      phone: body.customer?.phone,
-      addresses: body.customer?.addresses,
-      accepts_marketing_updated_at: body.customer?.accepts_marketing_updated_at,
-      marketing_opt_in_level: body.customer?.marketing_opt_in_level,
-      tax_exemptions: body.customer?.tax_exemptions,
-      email_marketing_consent: body.customer?.email_marketing_consent,
-      sms_marketing_consent: body.customer?.sms_marketing_consent,
-      admin_graphql_api_id: body.customer?.admin_graphql_api_id,
-      default_address: body.customer?.default_address,
+      id: body.customer.id,
+      email: body.customer.email,
+      created_at: body.customer.created_at,
+      updated_at: body.customer.updated_at,
+      first_name: body.customer.first_name,
+      last_name: body.customer.last_name,
+      orders_count: body.customer.orders_count,
+      state: body.customer.state,
+      total_spent: body.customer.total_spent,
+      last_order_id: body.customer.last_order_id,
+      note: body.customer.note,
+      verified_email: body.customer.verified_email,
+      multipass_identifier: body.customer.multipass_identifier,
+      tax_exempt: body.customer.tax_exempt,
+      tags: body.customer.tags,
+      last_order_name: body.customer.last_order_name,
+      currency: body.customer.currency,
+      phone: body.customer.phone,
+      addresses: body.customer.addresses,
+      tax_exemptions: body.customer.tax_exemptions,
+      email_marketing_consent: body.customer.email_marketing_consent,
+      sms_marketing_consent: body.customer.sms_marketing_consent,
+      admin_graphql_api_id: body.customer.admin_graphql_api_id,
+      default_address: body.customer.default_address,
     },
   };
 
